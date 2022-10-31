184735c4558b8a10932c3095eed7e0bfd371b729
Noam Yan <noamyan9@gmail.com>
2022-10-31 09:27:49 -0500

    Cover page done.

--- app/src/main/java/com/example/shopmaster/CoverActivity.java
+++ app/src/main/java/com/example/shopmaster/CoverActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,12 +14,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CoverActivity extends AppCompatActivity {
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cover);
 
+        // Display the Cover for 2.5 seconds and then move to the Main Page.
+        mHandler.postDelayed(() -> {
+            Intent intent = new Intent(CoverActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2500);
     }
 
+    public void onBackPressed() {
+
+    }
 }
